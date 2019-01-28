0096d2685b315ff3cc142e93985fb925044eb3be
Geoff Bourne <itzgeoff@gmail.com>
2019-01-27 22:23:27 -0600

    Revert "Fix registration Excel view resolver"
    This reverts commit 82da9902

--- src/main/java/me/itzg/kidsbank/config/WebConfig.java
+++ src/main/java/me/itzg/kidsbank/config/WebConfig.java
@@ -1,6 +1,11 @@
 package me.itzg.kidsbank.config;
 
+import java.util.Locale;
+import me.itzg.kidsbank.web.ExcelView;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +21,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("/index.html");
         registry.addViewController("/parent/**").setViewName("/index.html");
         registry.addViewController("/kid/**").setViewName("/index.html");
+    }
+
+    @Bean
+    public ViewResolver excelViewResolver() {
+        return new ViewResolver() {
+            @Override
+            public View resolveViewName(String viewName, Locale locale) throws Exception {
+                return new ExcelView();
+            }
+        };
     }
 }
